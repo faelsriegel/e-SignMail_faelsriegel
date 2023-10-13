e03cda1477def39ea88e228b48f96f358b02d13a
Rafael Stuepp Riegel <rafaelsriegel@gmail.com>
2023-10-13 14:10:53 -0300

    teste

---
+++ scripts/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5500; // Alterado para a porta 5500
+const path = require('path');
+const session = require('express-session'); // Importe a biblioteca
+
+const users = [
+  { username: 'teste', password: 'teste@' },
+  // Adicione mais usuários conforme necessário
+];
+
+app.use(express.urlencoded({ extended: false }));
+
+// Configuração de sessão
+app.use(session({
+  secret: 'sua_chave_secreta', // Substitua por uma chave secreta segura
+  resave: false,
+  saveUninitialized: true,
+}));
+
+// Rota para a página de login
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Rota para processar o formulário de login
+app.post('/index', (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find((u) => u.username === username && u.password === password);
+
+  if (user) {
+    // Defina a sessão como autenticada
+    req.session.authenticated = true;
+    res.redirect('/main.html');
+  } else {
+    res.redirect('/index.html');
+  }
+});
+
+// Rota para a página principal (protegida)
+app.get('/main.html', (req, res) => {
+  // Verifique se o usuário está autenticado usando a sessão
+  if (req.session.authenticated) {
+    res.sendFile(path.join(__dirname, 'public/main.html'));
+  } else {
+    res.redirect('/index.html');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
