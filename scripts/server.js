d84d467273c48e240678cbd4d3d190a48a4ad58e
Rafael Stuepp Riegel <rafaelsriegel@gmail.com>
2023-10-13 16:08:40 -0300

    Teste Deploy

--- scripts/server.js
+++ scripts/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5500;
-
 const path = require('path');
+const session = require('express-session');
 
 const users = [
   { username: 'teste', password: 'teste@' }
@@ -11,27 +11,37 @@ const users = [
 
 app.use(express.urlencoded({ extended: false }));
 
+// Configuração de sessão
+app.use(session({
+  secret: 'sua_chave_secreta', // Substitua por uma chave secreta segura
+  resave: false,
+  saveUninitialized: true,
+}));
+
+// Rota para a página de login
 app.get('/index.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+// Rota para processar o formulário de login
 app.post('/index', (req, res) => {
   const { username, password } = req.body;
 
   const user = users.find((u) => u.username === username && u.password === password);
 
   if (user) {
-    // Adicione um parâmetro de autenticação à URL
-    res.redirect(302, '/main.html?authenticated=true');
+    // Defina a sessão como autenticada
+    req.session.authenticated = true;
+    res.redirect(302, '/main.html'); // Redirecionamento com status code 302
   } else {
     res.redirect('/index.html');
   }
 });
 
+// Rota para a página principal (protegida)
 app.get('/main.html', (req, res) => {
-  const isAuthenticated = req.query.authenticated === 'true';
-
-  if (isAuthenticated) {
+  // Verifique se o usuário está autenticado usando a sessão
+  if (req.session.authenticated) {
     res.sendFile(path.join(__dirname, 'public/main.html'));
   } else {
     res.redirect('/index.html');
